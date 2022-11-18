6d445559791938a95e233ae50e81af5c5e39d01e
David Bage <david.bage@snappyshopper.co.uk>
2022-11-18 21:04:57 +0000

    Initial Commit

---
+++ Matches Fashion Test App/Matches_Fashion_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matches_Fashion_Test_AppApp.swift
+//  Matches Fashion Test App
+//
+//  Created by David Bage on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Matches_Fashion_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
